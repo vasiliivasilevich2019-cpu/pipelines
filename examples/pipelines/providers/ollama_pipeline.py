121d765b0762c05c9facf2b11c17b9f9fb1cd4c7
vasiliivasilevich2019-cpu <vasilii.vasilevich.2019@gmail.com>
2025-11-03 20:33:19 +0300

    Update ollama_pipeline.py

--- examples/pipelines/providers/ollama_pipeline.py
+++ examples/pipelines/providers/ollama_pipeline.py
@@ -29,7 +29,7 @@ class Pipeline:
         # This is where you can add your custom pipelines like RAG.
         print(f"pipe:{__name__}")
 
-        OLLAMA_BASE_URL = "http://app:11434"
+        OLLAMA_BASE_URL = "http://localhost:11434"
         MODEL = "gpt-oss"
 
         if "user" in body:
